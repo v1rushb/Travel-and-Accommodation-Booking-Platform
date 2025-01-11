7459b778fa10c3a8b318fccda349f8d7a732970b
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-11 13:41:17 +0200

    Add UserRepository Interface

---
+++ src/TABP.Domain/Abstractions/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+using TABP.Domain.Models.User;
+
+namespace TABP.Domain.Abstractions.Repositories;
+
+public interface IUserRepository
+{
+    Task<Guid> AddAsync(UserDTO newUser);
+
+    Task<UserDTO> GetByIdAsync(Guid Id);
+
+    Task<bool> ExistsAsync(Guid Id);
+}
