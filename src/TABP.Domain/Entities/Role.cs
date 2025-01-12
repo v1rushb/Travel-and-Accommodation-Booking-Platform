40d5b0a34aca1ef1e9414ced4eea5f92d8543a69
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-12 11:25:30 +0200

    Add Role Navigation Properties

--- src/TABP.Domain/Entities/Role.cs
+++ src/TABP.Domain/Entities/Role.cs
@@ -6,4 +6,6 @@ public class Role : Entity
 {
     public string Name { get; set; }
     public string Description { get; set; }
+
+    public ICollection<User> Users { get; set; } = new List<User>();
 }
