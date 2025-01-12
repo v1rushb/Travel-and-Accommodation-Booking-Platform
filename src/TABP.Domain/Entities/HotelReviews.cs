cec457398087e256c3ed24d46593a200c286f2d2
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-12 10:50:49 +0200

    Add HotelReviews Entity

---
+++ src/TABP.Domain/Entities/HotelReviews.cs
@@ -0,0 +1,13 @@
+using TABP.Domain.Abstractions;
+using TABP.Domain.Enums;
+
+namespace TABP.Domain.Entities;
+
+public class HotelReviews : Entity
+{
+    public string Feedback { get; set; }
+    public HotelRating Rating { get; set; }
+    public bool IsVerifiedPurchase { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime ModificationDate { get; set; }
+}
