cb9da6e509185c7c998f32dabf6915e093750e2d
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-12 11:40:37 +0200

    Add Discount Navigation Properies

--- src/TABP.Domain/Entities/Discount.cs
+++ src/TABP.Domain/Entities/Discount.cs
@@ -8,4 +8,7 @@ public class Discount : Entity
     public DateTime StartingDate { get; set; }
     public DateTime EndingDate { get; set; }
     public decimal AmountPercentage { get; set; } 
+
+    public Guid HotelId { get; set; }
+    public Hotel Hotel { get; set; }
 }
