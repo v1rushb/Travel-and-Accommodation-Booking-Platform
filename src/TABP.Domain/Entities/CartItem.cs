12c90a4aedd561645424fc0af07552c9bc8e5d47
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-12 11:39:15 +0200

    Add CartItem Navigation Properies

--- src/TABP.Domain/Entities/CartItem.cs
+++ src/TABP.Domain/Entities/CartItem.cs
@@ -5,4 +5,9 @@ namespace TABP.Domain.Entities;
 public class CartItem : Entity
 {
     public DateTime CreationDate { get; set; }
+
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+    public Guid RoomId { get; set; }
+    public Room Room { get; set; }
 }
