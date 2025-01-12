7274480a2e143f2c00b4b6df4ed63671a1e2af2d
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-12 10:53:32 +0200

    Add HotelVists Entity (No Relationships)

---
+++ src/TABP.Domain/Entities/HotelVisits.cs
@@ -0,0 +1,12 @@
+using TABP.Domain.Abstractions;
+
+namespace TABP.Domain.Entities;
+
+// consider doing some enhancements to this entity.
+public class HotelVisits : Entity
+{
+    public DateTime VisitDate { get; set; } // maybe consider using "Date" instead of VisitDate?
+    public DateTime TimeSpent { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime ModificationDate { get; set; }
+}
