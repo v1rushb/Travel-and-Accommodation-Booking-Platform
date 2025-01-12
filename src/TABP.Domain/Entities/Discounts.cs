2c2522bf3d5b428076629244d1ee800594dec5fa
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-12 11:00:39 +0200

    Add Discount Entity (No Relationships)

---
+++ src/TABP.Domain/Entities/Discounts.cs
@@ -0,0 +1,11 @@
+using TABP.Domain.Abstractions;
+
+namespace TABP.Domain.Entities;
+
+public class Discount : Entity
+{
+    public string Reason { get; set; }
+    public DateTime StartingDate { get; set; }
+    public DateTime EndingDate { get; set; }
+    public decimal AmountPercentage { get; set; } 
+}
