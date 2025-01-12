2491fe8f196804afed9932c0935ae81b96866894
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-12 11:39:59 +0200

    Add HotelVisit Navigation Properies

--- src/TABP.Domain/Entities/HotelVisit.cs
+++ src/TABP.Domain/Entities/HotelVisit.cs
@@ -9,4 +9,9 @@ public class HotelVisit : Entity
     public DateTime TimeSpent { get; set; }
     public DateTime CreationDate { get; set; }
     public DateTime ModificationDate { get; set; }
+
+    public Guid HotelId { get; set; }
+    public Hotel Hotel { get; set; }
+    public Guid UserId { get; set; }
+    public User User { get; set; }
 }
