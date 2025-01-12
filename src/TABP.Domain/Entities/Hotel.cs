42b3a2644a5eeeb3bd2ec291074a76f702c8386c
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-12 11:32:48 +0200

    Add Hotel Navigation Properies

--- src/TABP.Domain/Entities/Hotel.cs
+++ src/TABP.Domain/Entities/Hotel.cs
@@ -13,4 +13,11 @@ public class Hotel : Entity
     // do hotel tags thingy
     public DateTime CreationDate { get; set; }
     public DateTime ModificationDate { get; set; }
+
+    public Guid CityId { get; set; }
+    public City City { get; set; }
+    public ICollection<Room> Rooms { get; set; } = new List<Room>();
+    public ICollection<Discount> Discounts { get; set; } = new List<Discount>();
+    public ICollection<HotelReview> HotelReviews { get; set; } = new List<HotelReview>();
+    public ICollection<HotelVisit> HotelVisits { get; set; } = new List<HotelVisit>();
 }
