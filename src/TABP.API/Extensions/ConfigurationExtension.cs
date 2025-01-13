d037a4d4bd1dbac143707aaf771a002f6a60ec3e
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-13 19:37:01 +0200

    Add Extension Method for cleaner IOptions pattern

---
+++ src/TABP.API/Extensions/ConfigurationExtension.cs
@@ -0,0 +1,15 @@
+using TABP.Domain.Models.Configurations;
+
+namespace TABP.API.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static IServiceCollection BindConfiguration(
+        this IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<ConnectionStrings>(
+            config.GetSection(nameof(ConnectionStrings)));
+        
+        return services;
+    }
+}
