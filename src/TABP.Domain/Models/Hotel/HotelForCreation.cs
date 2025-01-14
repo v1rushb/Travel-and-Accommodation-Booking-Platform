2e326543d82f868e3e7311b3d3d84582036c7cfa
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-14 17:27:57 +0200

    Add Hotel DTOs

---
+++ src/TABP.Domain/Models/Hotel/HotelForCreation.cs
@@ -0,0 +1,16 @@
+using TABP.Domain.Abstractions;
+
+namespace TABP.Domain.Models.Hotels;
+
+public class HotelForCreationDTO
+{
+    public string Name { get; set; }
+    public string BriefDescription { get; set; }
+    public string DetailedDescription { get; set; }
+    public decimal StarRating { get; set; }
+    public string OwnerName { get; set; }
+    public string Geolocation { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime ModificationDate { get; set; }
+    public Guid CityId { get; set; }
+}
