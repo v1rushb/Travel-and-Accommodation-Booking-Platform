58ef380b01a06e1ddf71797671e40e554881c322
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-16 14:22:28 +0200

    Add extra methods on contract

--- src/TABP.Domain/Abstractions/Services/IHotelReviewService.cs
+++ src/TABP.Domain/Abstractions/Services/IHotelReviewService.cs
@@ -9,6 +9,12 @@ public interface IHotelReviewService
     Task<HotelReview> GetByIdAsync(Guid reviewId);
     Task UpdateAsync(HotelReviewDTO updatedReview);
     Task DeleteAsync(Guid reviewId);
+
+    Task<bool> ExistsAsync(Guid Id);
     Task<bool> ExistsByUserAndHotelAsync(Guid userId, Guid HotelId);
     Task<decimal> GetReviewsByHotelCountAsync(Guid hotelId);
+    Task<double> GetAverageRatingByHotelAsync(Guid hotelId);
+
+    Task<IEnumerable<HotelReview>> GetReviewsByHotelAsync(Guid hotelId);
+    Task<IEnumerable<HotelReview>> GetReviewsByUserAsync(Guid userId);
 }
