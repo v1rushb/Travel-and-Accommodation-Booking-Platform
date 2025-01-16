badbd31c1f4634aba22cd94ccfd9eeb81be3c85d
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-16 14:22:04 +0200

    Implement extra methods added on contract (Repository Layer)

--- src/TABP.Infrastructure/Repositories/HotelReviewRepository.cs
+++ src/TABP.Infrastructure/Repositories/HotelReviewRepository.cs
@@ -26,11 +26,14 @@ public class HotelReviewsRepository : IHotelReviewRepository
     public async Task<Guid> AddAsync(HotelReviewDTO newReview)
     {
         var review = _mapper.Map<HotelReview>(newReview);
+        review.CreationDate = DateTime.UtcNow;
+        review.ModificationDate = DateTime.UtcNow;
 
         var entityEntry = _context.HotelReviews.Add(review);
         await _context.SaveChangesAsync();
 
-        _logger.LogInformation($"Created Review with Id: {review.Id}"); // who?
+        _logger.LogInformation("Created Review with Id: {ReviewId}, HotelId: {HotelId}, UserId: {UserId}", review.Id, review.HotelId, review.UserId) // who?
+        
         return entityEntry.Entity.Id;
     }
 
@@ -40,14 +43,18 @@ public class HotelReviewsRepository : IHotelReviewRepository
     public async Task UpdateAsync(HotelReviewDTO updatedReview)
     {
         var review = _mapper.Map<HotelReview>(updatedReview);
+        review.ModificationDate = DateTime.UtcNow;
+
         _context.HotelReviews.Update(review);
         await _context.SaveChangesAsync();
+         _logger.LogInformation("Updated Review with Id: {ReviewId}", updatedReview.Id);
     }
 
     public async Task DeleteAsync(Guid reviewId)
     {
         _context.HotelReviews.Remove(new HotelReview { Id = reviewId });
         await _context.SaveChangesAsync();
+        _logger.LogInformation("Deleted Review with Id: {ReviewId}", reviewId);
     }
 
     public async Task<bool> ExistsByUserAndHotelAsync(Guid userId, Guid HotelId) =>
@@ -55,4 +62,22 @@ public class HotelReviewsRepository : IHotelReviewRepository
 
     public async Task<decimal> GetReviewsByHotelCountAsync(Guid hotelId) =>
         await _context.HotelReviews.CountAsync(review => review.HotelId == hotelId);
+
+    public async Task<double> GetAverageRatingByHotelAsync(Guid hotelId) =>
+        await _context.HotelReviews
+            .Where(review => review.HotelId == hotelId)
+            .Select(review => (int) review.Rating)
+            .AverageAsync(rating => rating);
+    
+    public async Task<IEnumerable<HotelReview>> GetReviewsByHotelAsync(Guid hotelId) =>
+        await _context.HotelReviews.Where(review => review.HotelId == hotelId).ToListAsync();
+
+    public async Task<IEnumerable<HotelReview>> GetReviewsByUserAsync(Guid userId) =>
+        await _context.HotelReviews
+            .Include(review => review.User)
+            .Where(review => review.UserId == userId)
+            .ToListAsync();
+    
+    public async Task<bool> ExistsAsync(Guid Id) =>
+        await _context.HotelReviews.AnyAsync(review => review.Id == Id);
 }
