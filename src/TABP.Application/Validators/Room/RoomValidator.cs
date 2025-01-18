14380621acd9df600824b22640fcc13f3856dc20
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-18 17:09:26 +0200

    Add partial Validators for Room

---
+++ src/TABP.Application/Validators/Room/RoomValidator.cs
@@ -0,0 +1,37 @@
+using System.Data;
+using FluentValidation;
+using TABP.Domain.Abstractions.Services;
+using TABP.Domain.Constants.Room;
+using TABP.Domain.Models.Room;
+
+namespace TABP.Application.Validators.Room;
+
+public class RoomValidator : AbstractValidator<RoomDTO>
+{
+    public RoomValidator(IHotelService hotelService)
+    {
+        RuleFor(room => room.Number)
+            .NotNull();
+        
+
+        RuleFor(room => room.AdultsCapacity)
+            .NotNull()
+            .InclusiveBetween(RoomConstants.MinAdultsCapacity, RoomConstants.MaxAdultsCapacity); 
+
+        RuleFor(room => room.ChildrenCapacity)
+            .NotNull()
+            .InclusiveBetween(RoomConstants.MinChildrenCapacity, RoomConstants.MaxChildrenCapacity);
+
+        RuleFor(room => room.PricePerNight)
+            .NotNull()
+            .GreaterThanOrEqualTo(RoomConstants.MinPricePerNight);
+        
+        RuleFor(room => room.HotelId)
+            .NotNull()
+            .MustAsync(async (id, cancellation) =>
+                await hotelService.ExistsAsync(id))
+            .WithMessage("{PropertyName} does not exist");
+        
+        // validate type.
+    }
+}
---
+++ src/TABP.Domain/Constants.cs/Room/RoomConstants.cs
@@ -0,0 +1,10 @@
+namespace TABP.Domain.Constants.Room;
+
+public static class RoomConstants
+{
+    public const int MinAdultsCapacity = 0;
+    public const int MaxAdultsCapacity = 15;
+    public const int MinChildrenCapacity = 0;
+    public const int MaxChildrenCapacity = 10;
+    public const int MinPricePerNight = 0;
+}
