a4143a051a78b69fc6877b7a40ee8547a45714e1
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-20 09:40:49 +0200

    Add City Searching/Response DTO

---
+++ src/TABP.Domain/Models/City/Search/CitySearchQuery.cs
@@ -0,0 +1,7 @@
+namespace TABP.Domain.Models.City.Search;
+
+public class CitySearchQuery
+{
+    public string? SearchTerm { get; set; }
+    public string? Country { get; set; }
+}
---
+++ src/TABP.Domain/Models/City/Search/CitySearchResponseDTO.cs
@@ -0,0 +1,9 @@
+using TABP.Domain.Models.Hotels;
+
+namespace TABP.Domain.Models.City.Search;
+
+public class CitySearchResponseDTO
+{
+    public string Name { get; set; }
+    public string CountryName { get; set; }
+}
