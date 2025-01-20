3f980fc22eeae6158a191bdee8183fbfbcc6f479
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-20 13:40:13 +0200

    Change room constants

--- src/TABP.Domain/Constants.cs/Room/RoomConstants.cs
+++ src/TABP.Domain/Constants.cs/Room/RoomConstants.cs
@@ -2,7 +2,7 @@ namespace TABP.Domain.Constants.Room;
 
 public static class RoomConstants
 {
-    public const int MinAdultsCapacity = 0;
+    public const int MinAdultsCapacity = 2;
     public const int MaxAdultsCapacity = 15;
     public const int MinChildrenCapacity = 0;
     public const int MaxChildrenCapacity = 10;
