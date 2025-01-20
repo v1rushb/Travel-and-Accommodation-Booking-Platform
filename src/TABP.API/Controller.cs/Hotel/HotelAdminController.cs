374bd79343d2297c95991e28ad50077147b49c50
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-20 11:41:14 +0200

    Add Hotel for Admin Controller (Paritally)

---
+++ src/TABP.API/Controller.cs/Hotel/HotelAdminController.cs
@@ -0,0 +1,104 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using TABP.Abstractions.Services;
+using TABP.API.Extensions;
+using TABP.Domain.Abstractions.Services;
+using TABP.Domain.Models.Hotel;
+using TABP.Domain.Models.Hotel.Search.Response;
+using TABP.Domain.Models.Hotels;
+using TABP.Domain.Models.Pagination;
+
+namespace TABP.API.Controllers;
+
+// [Authorize] // add admin later.
+[ApiController]
+[Route("api/hotel/admin")]
+public class HotelAdminController : ControllerBase
+{
+    private readonly IHotelService _hotelService;
+    private readonly IMapper _mapper;
+    private readonly IRoomService _roomService;
+    private readonly IDiscountService _discountService;
+
+    public HotelAdminController(
+        IHotelService hotelService,
+        IMapper mapper,
+        IRoomService roomService,
+        IDiscountService discountService)
+    {
+        _hotelService = hotelService;
+        _mapper = mapper;
+        _roomService = roomService;
+        _discountService = discountService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateHotelAsync([FromBody] HotelForCreationDTO newHotel)
+    {
+        // do some high level exception handling.
+
+        var hotelId = await _hotelService.AddAsync(_mapper.Map<HotelDTO>(newHotel));
+
+        return Created();
+    }
+
+    [HttpPatch("{hotelId:guid}")]
+    public async Task<IActionResult> PatchHotelAsync(
+        Guid hotelId,
+        [FromBody] JsonPatchDocument<HotelForUpdateDTO> patchDoc)
+    {
+        var hotel = await _hotelService.GetByIdAsync(hotelId);
+        var hotelToPartiallyUpdate = GetHotelForPartialUpdate(patchDoc, hotel);
+
+        if(!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        _mapper.Map(hotelToPartiallyUpdate, hotel);
+        await _hotelService.UpdateAsync(hotel);
+
+        return NoContent();
+    }
+
+    private HotelForUpdateDTO GetHotelForPartialUpdate(
+        JsonPatchDocument<HotelForUpdateDTO> patchDoc,
+        HotelDTO city)
+    {
+        var cityToUpdate = _mapper.Map<HotelForUpdateDTO>(city);
+        patchDoc.ApplyTo(cityToUpdate, ModelState);
+
+        return cityToUpdate;
+    }
+
+    [HttpDelete("{hotelId:guid}")]
+    public async Task<IActionResult> DeleteHotelAsync(Guid hotelId)
+    {
+        await _hotelService.DeleteAsync(hotelId);
+
+        return NoContent();
+    }
+
+    [HttpGet("search")]
+    public async Task<IActionResult> SearchHotelsForAdminAsync(
+        [FromQuery] PaginationDTO pagination,
+        [FromQuery] HotelSearchQuery query)
+    {
+        var hotels = await _hotelService.SearchAdminAsync(query, pagination);
+        var hotelCount = hotels.Count();
+
+        Response.Headers.AddPaginationHeaders(hotelCount, pagination);
+
+        return Ok(hotels);
+    }
+
+    [HttpGet("{hotelId:guid}")]
+    public async Task<IActionResult> SearchHotelById(Guid hotelId)
+    {
+        var hotel = await _hotelService.GetByIdAsync(hotelId);
+            _mapper.Map<HotelAdminWithoutIdResponseDTO>(hotel);
+        return Ok(hotel);
+    }
+}
---
+++ src/TABP.Domain/Models/Hotel/HotelForUpdateDTO.cs
@@ -0,0 +1,12 @@
+namespace TABP.Domain.Models.Hotel;
+
+public class HotelForUpdateDTO
+{
+    public string Name { get; set; }
+    public string BriefDescription { get; set; }
+    public string DetailedDescription { get; set; }
+    public decimal StarRating { get; set; }
+    public string OwnerName { get; set; }
+    public string Geolocation { get; set; }
+    public Guid CityId { get; set; }
+}
