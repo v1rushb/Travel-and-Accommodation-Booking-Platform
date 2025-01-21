c65e27305eff39e61eaefd6229809cc44fee2e76
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-21 21:30:06 +0200

    Add & Implement Cart Repository

---
+++ src/TABP.Domain/Abstractions/Repositories/ICartRepository.cs
@@ -0,0 +1,22 @@
+using TABP.Domain.Models.Cart;
+using TABP.Domain.Models.CartItem;
+
+namespace TABP.Domain.Abstractions.Repositories;
+
+public interface ICartRepository
+{
+    Task<Guid> CreateAsync(CartDTO cart);
+    Task DeleteAsync(Guid Id);
+    Task UpdateAsync(CartDTO updatedCart);
+    Task<bool> ExistsAsync(Guid Id);
+    Task<CartDTO> GetByIdAsync(Guid Id);
+    Task<CartDTO> GetLastPendingCartAsync(Guid userId);
+    Task AddItemAsync(CartItemDTO newCartItem);
+    Task DeleteItemAsync(Guid cartItemId);
+    Task UpdateItemAsync(CartItemDTO cartItem);
+
+    Task<IEnumerable<CartItemDTO>> GetAllCartItemsAsync(Guid cartId, int pageNumber, int pageSize);
+    Task<bool> RoomIsBookedForSameUserBetween(Guid roomId, Guid userId, DateTime StartingDate, DateTime EndingDate);
+    Task<bool> RoomIsBookedByAnyUserBetween(Guid roomId, DateTime StartingDate, DateTime EndingDate);
+    // do search later.
+}
---
+++ src/TABP.Infrastructure/Repositories/CartRepository.cs
@@ -0,0 +1,158 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TABP.Domain.Abstractions.Repositories;
+using TABP.Domain.Entities;
+using TABP.Domain.Enums;
+using TABP.Domain.Models.Cart;
+using TABP.Domain.Models.CartItem;
+using TABP.Infrastructure.Extensions.Helpers;
+
+namespace TABP.Infrastructure.Repositories;
+
+public class CartRepository : ICartRepository
+{
+    private readonly HotelBookingDbContext _context;
+    private readonly IMapper _mapper;
+    private readonly ILogger<CartRepository> _logger;
+
+    public CartRepository(
+        HotelBookingDbContext context,
+        IMapper mapper,
+        ILogger<CartRepository> logger)
+    {
+        _context = context;
+        _mapper = mapper;
+        _logger = logger;
+    }
+
+    public async Task<Guid> CreateAsync(CartDTO newCartItem)
+    {
+        var cart = _mapper.Map<Cart>(newCartItem); // add mapping here. ----------------------------------------------------
+        
+        var entity = await _context.Carts.AddAsync(cart);
+        await _context.SaveChangesAsync();
+
+         _logger.LogInformation("Created Cart with Id: {CartId} for User: {UserId}", entity.Entity.Id, cart.UserId);
+
+        return entity.Entity.Id;
+    }
+
+    public async Task DeleteAsync(Guid Id)
+    {
+        _context.Carts.Remove(new Cart { Id = Id });
+        await _context.SaveChangesAsync();
+
+        _logger.LogInformation("Deleted Cart with Id: {CartId}", Id);
+    }
+
+    public async Task<bool> ExistsAsync(Guid Id) =>
+        await _context.Carts.AnyAsync(cart => cart.Id == Id);
+
+    public async Task<CartDTO> GetByIdAsync(Guid Id)
+    {
+        var cart = await _context.Carts
+            .Include(c => c.Items)
+            .FirstOrDefaultAsync(c => c.Id == Id);
+
+        return _mapper.Map<CartDTO>(cart);
+    }
+
+    public async Task<CartDTO> GetLastPendingCartAsync(Guid userId)
+    {
+        var cart = await _context.Carts
+            .Include(cart => cart.Items)
+            .Where(cart => cart.UserId == userId && cart.Status == BookingStatus.Pending)
+            .OrderByDescending(cart => cart.CreationDate)
+            .FirstOrDefaultAsync();
+
+        return _mapper.Map<CartDTO>(cart);
+    }
+
+    public async Task AddItemAsync(CartItemDTO newItem)
+    {
+        var cartItem = _mapper.Map<CartItem>(newItem);
+        cartItem.CreationDate = DateTime.UtcNow;
+
+        _context.CartItems.Add(cartItem);
+        await _context.SaveChangesAsync();
+
+        _logger.LogInformation("Added Room {RoomId} to Cart {CartId}", cartItem.RoomId, cartItem.CartId); 
+    }
+
+    public async Task DeleteItemAsync(Guid cartItemId)
+    {
+        _context.CartItems.Remove(new CartItem { Id = cartItemId });
+        await _context.SaveChangesAsync();
+
+        _logger.LogInformation("Deleted CartItem with Id: {CartItemId}", cartItemId);
+    }
+
+    public async Task UpdateItemAsync(CartItemDTO cartItem)
+    {
+        var cartItemToUpdate = _mapper.Map<CartItem>(cartItem);
+        _context.CartItems.Update(cartItemToUpdate);
+
+        await _context.SaveChangesAsync();
+
+        _logger.LogInformation("Updated CartItem with Id: {CartItemId}", cartItem.Id);
+    }
+
+    public async Task<IEnumerable<CartItemDTO>> GetAllCartItemsAsync(
+        Guid cartId,
+        int pageNumber,
+        int pageSize)
+    {
+        var cartItems = await _context.Carts
+            .Where(cart => cart.Id == cartId)
+            .SelectMany(cart => cart.Items)
+            .PaginateAsync(pageNumber, pageSize);
+
+        // foreach (var cartItem in cartItems)
+        // {
+        //     System.Console.WriteLine(cartItem.);
+        // }
+
+        return _mapper.Map<IEnumerable<CartItemDTO>>(cartItems);
+    }
+
+    public async Task UpdateAsync(CartDTO updatedCart)
+    {
+        var cart = _mapper.Map<Cart>(updatedCart);
+
+        _context.Carts.Update(cart);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> RoomIsBookedForSameUserBetween(
+        Guid roomId,
+        Guid userId,
+        DateTime startingDate,
+        DateTime endingDate)
+    {
+        return await _context.Carts
+            .Where(cart => cart.UserId == userId && cart.Status == BookingStatus.Pending)
+            .SelectMany(cart => cart.Items)
+            .AnyAsync(cartItem =>
+                cartItem.RoomId == roomId &&
+                cartItem.CheckInDate < endingDate &&
+                cartItem.CheckOutDate > startingDate);
+    }
+
+    public async Task<bool> RoomIsBookedByAnyUserBetween(
+        Guid roomId,
+        DateTime startingDate,
+        DateTime endingDate)
+    {
+        return await _context.Carts
+            .Where(cart => cart.Status == BookingStatus.Confirmed || cart.Status == BookingStatus.Pending)
+            .SelectMany(cart => cart.Items)
+            .AnyAsync(cartItem =>
+                cartItem.RoomId == roomId &&
+                cartItem.CheckInDate < endingDate &&
+                cartItem.CheckOutDate > startingDate);
+    }
+
+
+}
