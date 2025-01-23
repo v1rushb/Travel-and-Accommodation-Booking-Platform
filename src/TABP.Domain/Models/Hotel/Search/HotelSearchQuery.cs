1af5224647f99fff888d2362a18953e1e7e772c6
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-23 12:57:26 +0200

    Modify Search Filters for Hotel

--- src/TABP.Domain/Models/Hotel/Search/HotelSearchQuery.cs
+++ src/TABP.Domain/Models/Hotel/Search/HotelSearchQuery.cs
@@ -1,19 +1,20 @@
 using TABP.Domain.Enums;
 
-namespace TABP.Domain.Models.Hotel;
+namespace TABP.Domain.Models.Hotel.Search;
 
 public class HotelSearchQuery
 {
     public string? SearchTerm { get; set; }
-    public DateTime CheckInDate { get; set; }
-    public DateTime CheckOutDate { get; set; }
+    public DateTime? CheckInDate { get; set; }
+    public DateTime? CheckOutDate { get; set; }
     public int NumberOfAdults { get; set; } = 2;
     public int NumberOfChildren { get; set; } = 0;
-    public int NumberOfRooms { get; set; }
-    public int MinPrice { get; set; }
-    public int MaxPrice { get; set; }
+    public int MinNumberOfRooms { get; set; } = 0;
+    public int MaxNumberOfRooms { get; set; } = int.MaxValue;
+    public int MinPricePerNight { get; set; } = 0;
+    public int MaxPricePerNight { get; set; } = int.MaxValue;
     public int? MinStars { get; set; }
     public int? MaxStars { get; set; }
     public string? City { get; set; }
-    public IEnumerable<RoomType>? RoomTypes { get; set; }
+    public IEnumerable<int>? RoomTypes { get; set; }
 }
