5a4d06ef768d83a8f4194fd2214e663cefe19a69
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-23 13:05:49 +0200

    Modify Filters for Room Search

--- src/TABP.Domain/Models/Room/Search/RoomSearchQuery.cs
+++ src/TABP.Domain/Models/Room/Search/RoomSearchQuery.cs
@@ -1,12 +1,14 @@
-using TABP.Domain.Enums;
-
 namespace TABP.Domain.Models.Room.Search;
 
 public class RoomSearchQuery
 {
-    public int Number { get; set; }
-    public RoomType Type { get; set; }
-    public int AdultsCapacity { get; set; } = 2;
-    public int ChildrenCapacity { get; set; } = 0;
-    public int PricePerNight { get; set; }
+    public int MinNumber { get; set; } = 1;
+    public int MaxNumber { get; set; } = int.MaxValue;
+    public IEnumerable<int> roomType { get; set; } 
+    public int MinAdultsCapacity { get; set; } = 2;
+    public int MaxAdultsCapacity { get; set; } = int.MaxValue;
+    public int MinChildrenCapacity { get; set; } = 0;
+    public int MaxChildrenCapacity { get; set; } = int.MaxValue;
+    public int MinPricePerNight { get; set; } = 0;
+    public int MaxPricePerNight { get; set; } = int.MaxValue;
 }
