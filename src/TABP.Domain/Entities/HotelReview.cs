62cc6041a18210e087c3c0d9b46138399aa1b1c4
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-23 12:47:54 +0200

    Remove metadata from HotelReview

--- src/TABP.Domain/Entities/HotelReview.cs
+++ src/TABP.Domain/Entities/HotelReview.cs
@@ -7,7 +7,6 @@ public class HotelReview : Entity
 {
     public string Feedback { get; set; }
     public HotelRating Rating { get; set; }
-    public bool IsVerifiedPurchase { get; set; }
     public DateTime CreationDate { get; set; }
     public DateTime ModificationDate { get; set; }
 
