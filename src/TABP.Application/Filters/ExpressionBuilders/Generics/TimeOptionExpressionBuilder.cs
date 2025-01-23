66abd35602408583a8b978a93cfcc254cd088911
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-23 20:29:19 +0200

    Add Generic Expression builder

---
+++ src/TABP.Application/Filters/ExpressionBuilders/Generics/TimeOptionExpressionBuilder.cs
@@ -0,0 +1,55 @@
+using System.Linq.Expressions;
+using TABP.Application.Extensions;
+using TABP.Domain.Abstractions.Services.Generics;
+using TABP.Domain.Entities;
+using TABP.Domain.Enums;
+using TABP.Domain.Models.HotelVisit;
+
+namespace TABP.Application.Filters.ExpressionBuilders.Generics;
+
+public static class TimeOptionExpressionBuilder<T> where T : class, IHasCreationDate
+{
+    public static Expression<Func<T, bool>> Build(VisitTimeOptionQuery query)
+    {
+        var filter = Expressions.True<T>();
+
+        filter =
+            filter.AndIf(
+                HasValidTimeOption(query),
+                GetTimeOptionFilter(query)
+            );
+
+        return filter;
+    }
+
+    private static bool HasValidTimeOption(VisitTimeOptionQuery query)
+    {
+        if(query.TimeOption.HasValue)
+        {
+            return Enum.IsDefined(typeof(TimeOptions), query.TimeOption.Value);
+        }
+        return false;
+    }
+
+    private static Expression<Func<T, bool>> GetTimeOptionFilter(VisitTimeOptionQuery query)
+    {
+        if(!query.TimeOption.HasValue)
+            return visit => true;
+        
+        var timeOption = Enum.Parse<TimeOptions>(query.TimeOption.ToString());
+
+        DateTime? filterDate = timeOption switch
+        {
+            TimeOptions.Today => DateTime.Now.Date,
+            TimeOptions.LastWeek => DateTime.Now.AddDays(-7).Date,
+            TimeOptions.LastMonth => DateTime.Now.AddMonths(-1).Date,
+            TimeOptions.LastYear => DateTime.Now.AddYears(-1).Date,
+            _ => null,
+        };
+
+        return filterDate.HasValue ? visit =>
+            visit.CreationDate.Date >= filterDate.Value
+            : visit => true;
+    }
+
+}
