5a1453d3034887152febd668b2636c81ad6b7fc7
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-23 13:06:48 +0200

    Modify Filters for Discount Search

--- src/TABP.Domain/Models/Discount/Search/DiscountSearchQuery.cs
+++ src/TABP.Domain/Models/Discount/Search/DiscountSearchQuery.cs
@@ -8,6 +8,7 @@ public class DiscountSearchQuery
     public string SearchTerm { get; set; }
     public DateTime StartingDate { get; set; }
     public DateTime EndingDate { get; set; }
-    public decimal AmountPercentage { get; set; }
-    public RoomType roomType { get; set; }
+    public decimal MinAmountPercentage { get; set; } = 0;
+    public decimal MaxAmountPercentage { get; set; } = 100;
+    public IEnumerable<int> RoomType { get; set; }
 }
