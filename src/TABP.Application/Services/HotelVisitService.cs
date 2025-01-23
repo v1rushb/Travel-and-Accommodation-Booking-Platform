af6cf293317837f651adccd0d94382e6438d84b7
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-23 20:26:06 +0200

    Implement TopFiveVisitedHotels method (service level)

--- src/TABP.Application/Services/HotelVisitService.cs
+++ src/TABP.Application/Services/HotelVisitService.cs
@@ -1,7 +1,10 @@
 using Microsoft.Extensions.Logging;
+using TABP.Application.Filters.ExpressionBuilders.Generics;
 using TABP.Domain.Abstractions.Repositories;
 using TABP.Domain.Abstractions.Services;
 using TABP.Domain.Entities;
+using TABP.Domain.Models.Hotel;
+using TABP.Domain.Models.Hotels;
 using TABP.Domain.Models.HotelVisit;
 
 namespace TABP.Application.Services;
@@ -9,33 +12,27 @@ namespace TABP.Application.Services;
 public class HotelVisitService : IHotelVisitService
 {
     private readonly IHotelVisitRepository _hotelVisitRepository;
+    private readonly ICurrentUserService _currentUserService;
     private readonly ILogger<HotelVisitService> _logger;
+
     
     public HotelVisitService(
         IHotelVisitRepository hotelVisitRepository,
-        ILogger<HotelVisitService> logger)
+        ILogger<HotelVisitService> logger,
+        ICurrentUserService currentUserService)
     {
         _hotelVisitRepository = hotelVisitRepository;
         _logger = logger;
+        _currentUserService = currentUserService;
     }
 
     public async Task<Guid> AddAsync(HotelVisitDTO newHotelVisit)
     {
-        // do validations here.
         var visitId = await _hotelVisitRepository.AddAsync(newHotelVisit);
 
         _logger.LogInformation("Added HotelVisit for HotelId: {HotelId}, UserId: {UserId}", newHotelVisit.HotelId, newHotelVisit.UserId);
         
         return visitId;
-    }
-
-    public async Task DeleteAsync(Guid Id)
-    {
-        await ValidateId(Id);
-
-        await _hotelVisitRepository.DeleteAsync(Id);
-
-        _logger.LogInformation("Deleted HotelVisit with Id: {Id}", Id);
     }
 
     public async Task<bool> ExistsAsync(Guid Id) =>
@@ -44,29 +41,17 @@ public class HotelVisitService : IHotelVisitService
     public async Task<IEnumerable<HotelVisit>> GetByHotelAsync(Guid HotelId, DateTime? startDate = null, DateTime? endDate = null) =>
         await _hotelVisitRepository.GetByHotelAsync(HotelId, startDate, endDate);
 
-    public async Task<HotelVisit> GetByIdAsync(Guid Id) =>
-        await _hotelVisitRepository.GetByIdAsync(Id);
-
     public async Task<IEnumerable<HotelVisit>> GetByUserAsync(Guid userId, DateTime? startDate = null, DateTime? endDate = null) =>
         await _hotelVisitRepository.GetByUserAsync(userId, startDate, endDate);
     
     public async Task<IEnumerable<HotelVisit>> GetByUserAndHotelAsync(Guid userId, Guid hotelId, DateTime? startDate = null, DateTime? endDate = null) =>
         await _hotelVisitRepository.GetByUserAndHotelAsync(userId, hotelId, startDate, endDate);
-    public async Task UpdateAsync(HotelVisitDTO updatedHotelVisit)
+
+    public async Task<IEnumerable<VisitedHotelDTO>> GetTop5VisitedHotels(VisitTimeOptionQuery query)
     {
-        await ValidateId(updatedHotelVisit.Id);
-        updatedHotelVisit.ModificationDate = DateTime.UtcNow;
+        var expression = TimeOptionExpressionBuilder<HotelVisit>.Build(query);
 
-        await _hotelVisitRepository.UpdateAsync(updatedHotelVisit);
-
-        _logger.LogInformation("Updated HotelVisit with Id: {Id}", updatedHotelVisit.Id);
-    }
-
-    private async Task ValidateId(Guid Id)
-    {
-        if(! await ExistsAsync(Id))
-        {
-            throw new KeyNotFoundException($"Id {Id} Does not exist.");
-        }
+        return await _hotelVisitRepository
+            .GetTop5VisitedHotels(expression);
     }
 }
--- src/TABP.Domain/Abstractions/Services/IHotelVisitService.cs
+++ src/TABP.Domain/Abstractions/Services/IHotelVisitService.cs
@@ -1,4 +1,6 @@
 using TABP.Domain.Entities;
+using TABP.Domain.Models.Hotel;
+using TABP.Domain.Models.Hotels;
 using TABP.Domain.Models.HotelVisit;
 
 namespace TABP.Domain.Abstractions.Services;
@@ -7,9 +9,8 @@ public interface IHotelVisitService
 {
     Task<Guid> AddAsync(HotelVisitDTO newHotelVisit);
     Task<bool> ExistsAsync(Guid Id);
-    Task UpdateAsync(HotelVisitDTO updatedHotelVisit);
-    Task DeleteAsync(Guid Id);
-    Task<HotelVisit> GetByIdAsync(Guid Id);
-    Task<IEnumerable<HotelVisit>> GetByUserAsync(Guid userId);
-    Task<IEnumerable<HotelVisit>> GetByHotelAsync(Guid HotelId);
+    Task<IEnumerable<HotelVisit>> GetByUserAsync(Guid userId, DateTime? startDate = null, DateTime? endDate = null);
+    Task<IEnumerable<HotelVisit>> GetByHotelAsync(Guid HotelId, DateTime? startDate = null, DateTime? endDate = null);
+    Task<IEnumerable<HotelVisit>> GetByUserAndHotelAsync(Guid userId, Guid hotelId, DateTime? startDate = null, DateTime? endDate = null);
+    Task<IEnumerable<VisitedHotelDTO>> GetTop5VisitedHotels(VisitTimeOptionQuery query);
 }
