591dceadd7e04525bd07cf6ee48163b2716fe184
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-23 12:46:23 +0200

    Rename folder

---
+++ src/TABP.Domain/Constants/Expression/HotelSearchQueryContasnts.cs
@@ -0,0 +1,10 @@
+namespace TABP.Domain.Constants.Expression;
+
+public static class HotelSearchQueryConstants
+{
+    public const int MinPrice = 0;
+    public const int MaxPrice = 0;
+    public const int NumberOfRooms = 0;
+    public const int NumberOfAdults = 0;
+    public const int NumberOfChildren = 0;
+}
