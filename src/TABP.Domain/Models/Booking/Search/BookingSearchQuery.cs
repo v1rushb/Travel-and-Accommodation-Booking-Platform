10a1e69ed7c68fbf97cbaf4766f432452e454237
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-23 12:53:02 +0200

    Modify Booking Search Query attributes

--- src/TABP.Domain/Models/Booking/Search/BookingSearchQuery.cs
+++ src/TABP.Domain/Models/Booking/Search/BookingSearchQuery.cs
@@ -1,17 +1,12 @@
-using TABP.Domain.Enums;
-
 namespace TABP.Domain.Models.Booking.Search;
 
 public class BookingSearchQuery
 {
     public DateTime? CheckInDate { get; set; }
     public DateTime? CheckOutDate { get; set; }
-    public DateTime? CheckInDateFrom { get; set; }
-    public DateTime? CheckInDateTo { get; set; }
-    public decimal? MinPrice { get; set; }
-    public decimal? MaxPrice { get; set; }
+    public decimal MinPrice { get; set; } = 0;
+    public decimal MaxPrice { get; set; } = decimal.MaxValue;
     public string Notes { get; set; }
-    public BookingStatus Status { get; set; }
-    public int RoomNumber { get; set; }
+    public int RoomNumber { get; set; } = 1;
     public Guid HotelId { get; set; }
 }
