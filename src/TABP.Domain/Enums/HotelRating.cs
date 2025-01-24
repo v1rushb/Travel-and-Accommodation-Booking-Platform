28276784c3e751252bc655f471084ef45e6561aa
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-24 10:50:28 +0200

    Modify HotelRating Enum

--- src/TABP.Domain/Enums/HotelRating.cs
+++ src/TABP.Domain/Enums/HotelRating.cs
@@ -2,9 +2,8 @@ namespace TABP.Domain.Enums;
 
 public enum HotelRating
 {
-    VeryPoor = 0,
-    Poor = 1,
-    Fair = 2,
+    VeryPoor = 1,
+    Poor = 2,
     Good = 3,
     VeryGood = 4,
     Excellent = 5
