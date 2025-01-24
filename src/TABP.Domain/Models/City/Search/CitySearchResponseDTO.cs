63ab3bddbe4bb3a18db395bb928be10842c4809d
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-24 11:04:53 +0200

    Include Id in CitySearchResponse

--- src/TABP.Domain/Models/City/Search/CitySearchResponseDTO.cs
+++ src/TABP.Domain/Models/City/Search/CitySearchResponseDTO.cs
@@ -1,8 +1,9 @@
+using TABP.Domain.Abstractions;
 using TABP.Domain.Models.Hotels;
 
 namespace TABP.Domain.Models.City.Search;
 
-public class CitySearchResponseDTO
+public class CitySearchResponseDTO : Entity
 {
     public string Name { get; set; }
     public string CountryName { get; set; }
