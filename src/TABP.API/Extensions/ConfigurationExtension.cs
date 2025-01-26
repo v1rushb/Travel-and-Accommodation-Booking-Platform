380d468789ee6ede5a8cf9dd4d2a53cd41ba8ff7
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-26 18:41:15 +0200

    Add Config for Cache Service & Email Service

--- src/TABP.API/Extensions/ConfigurationExtension.cs
+++ src/TABP.API/Extensions/ConfigurationExtension.cs
@@ -7,13 +7,18 @@ public static class ConfigurationExtensions
     public static IServiceCollection BindConfiguration(
         this IServiceCollection services, IConfiguration config)
     {
-        services.Configure<ConnectionStrings>(
-            config.GetSection(nameof(ConnectionStrings))
-        );
+        services.AddOptions<ConnectionStrings>()
+            .Bind(config.GetSection(nameof(ConnectionStrings)))
+            .ValidateOnStart();
+
 
         services.Configure<JWTConfigurations>(
             config.GetSection(nameof(JWTConfigurations))   
         );
+
+        services.AddOptions<EmailSettings>()
+            .Bind(config.GetSection(nameof(EmailSettings)))
+            .ValidateOnStart();
         
         return services;
     }
