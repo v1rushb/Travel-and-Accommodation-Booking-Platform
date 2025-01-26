c3526e083c29cb8375117b7a267e38519feda6f7
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-26 18:48:42 +0200

    Fix logical error

--- src/TABP.Application/Validators/Cart/CartValidator.cs
+++ src/TABP.Application/Validators/Cart/CartValidator.cs
@@ -16,13 +16,29 @@ public class CartItemValidator : AbstractValidator<CartItemDTO>
         _cartRepository = cartRepository;
         _currentUserService = currentUserService;   
 
-        RuleFor(cartItem => cartItem)
-            .MustAsync(RoomNotAlreadyBooked)
-            .WithMessage("Room is already booked for this period.");
+        RuleFor(cartItem => cartItem.CheckInDate)
+            .GreaterThan(DateTime.UtcNow)
+            .WithMessage("Check-in date must be in the future.");
+
+        RuleFor(cartItem => cartItem.CheckOutDate)
+            .GreaterThan(cartItem => cartItem.CheckInDate)
+            .WithMessage("Check-out date must be after Check-in date.");
 
         RuleFor(cartItem => cartItem)
-            .MustAsync(RoomNotAlreadyBookedByAnyUser)
-            .WithMessage("This room is already booked during the selected dates.");
+            .MustAsync(async (cartItem, cancellationToken) =>
+            {
+                if (!await RoomNotAlreadyBookedByAnyUser(cartItem, cancellationToken))
+                    return false;
+
+                return await RoomNotAlreadyBooked(cartItem, cancellationToken);
+            })
+            .WithMessage(cartItem =>
+            {
+                return !RoomNotAlreadyBookedByAnyUser(cartItem, CancellationToken.None).Result
+                    ? "This room is already booked during the selected dates."
+                    : "You already have a booking with this room within the selected dates.";
+            });
+        
     }
 
     private async Task<bool> RoomNotAlreadyBooked(CartItemDTO cartItem, CancellationToken cancellationToken)
