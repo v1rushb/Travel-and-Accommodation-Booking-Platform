cc55f153d2a2fc30f39bc79d17f66824678f74e4
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-26 18:41:44 +0200

    Add Redis ConnectionString

--- src/TABP.Domain/Models/Configurations/ConnectionStrings.cs
+++ src/TABP.Domain/Models/Configurations/ConnectionStrings.cs
@@ -2,5 +2,6 @@ namespace TABP.Domain.Models.Configurations;
 
 public class ConnectionStrings
 {
-    public string SQLString { get; set; }
+    public required string SQLString { get; set; }
+    public required string Redis { get; set; }
 }
