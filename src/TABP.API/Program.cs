bc4e763fd61bbaeb255b45b54eb2f9fc3e7af827
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-28 11:48:31 +0200

    Fix middleware pipeline in Program.cs

--- src/TABP.API/Program.cs
+++ src/TABP.API/Program.cs
@@ -38,8 +38,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthentication();
-app.UseAuthorization();
 app.UseMiddleware<Blacklist>();
+app.UseAuthorization();
 app.UseExceptionHandler();
 
 
