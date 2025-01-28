a6d084d136534b99e779466bc317e54dc26e5f07
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-28 11:53:20 +0200

    Take off Rating attribute for hotel creation and update

--- src/TABP.Domain/Models/Hotel/HotelForCreationDTO.cs
+++ src/TABP.Domain/Models/Hotel/HotelForCreationDTO.cs
@@ -7,7 +7,6 @@ public class HotelForCreationDTO
     public string Name { get; set; }
     public string BriefDescription { get; set; }
     public string DetailedDescription { get; set; }
-    public decimal StarRating { get; set; }
     public string OwnerName { get; set; }
     public string Geolocation { get; set; }
     public Guid CityId { get; set; }
--- src/TABP.Domain/Models/Hotel/HotelForUpdateDTO.cs
+++ src/TABP.Domain/Models/Hotel/HotelForUpdateDTO.cs
@@ -5,7 +5,6 @@ public class HotelForUpdateDTO
     public string Name { get; set; }
     public string BriefDescription { get; set; }
     public string DetailedDescription { get; set; }
-    public decimal StarRating { get; set; }
     public string OwnerName { get; set; }
     public string Geolocation { get; set; }
     public Guid CityId { get; set; }
