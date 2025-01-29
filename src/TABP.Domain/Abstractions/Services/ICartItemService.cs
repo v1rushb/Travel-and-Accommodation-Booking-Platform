ae25811ef3643eec96042d22f66ef8dd07fbb5de
v1rushb <cs.bashar.herbawi@gmail.com>
2025-01-29 15:21:12 +0200

    Change Contract method signature

--- src/TABP.Domain/Abstractions/Services/ICartItemService.cs
+++ src/TABP.Domain/Abstractions/Services/ICartItemService.cs
@@ -1,11 +1,10 @@
-using TABP.Domain.Entities;
 using TABP.Domain.Models.CartItem;
 
 namespace TABP.Domain.Abstractions.Services;
 
 public interface ICartItemService
 {
-    Task<Guid> AddAsync(CartItemDTO newCartItem);
+    Task AddAsync(CartItemDTO newCartItem);
     Task DeleteAsync(Guid Id);
     Task<CartItemDTO> GetByIdAsync(Guid Id);
     Task<bool> ExistsAsync(Guid Id);
