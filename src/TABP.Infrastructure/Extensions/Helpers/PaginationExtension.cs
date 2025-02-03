99c0ac452b4facaa1573234e82b2fdaa20d57e11
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-03 16:06:42 +0200

    Add Non-Async Pagination for IQueryable

--- src/TABP.Infrastructure/Extensions/Helpers/PaginationExtension.cs
+++ src/TABP.Infrastructure/Extensions/Helpers/PaginationExtension.cs
@@ -28,4 +28,14 @@ public static class PaginationExtension
 
         return Items;
     }
+
+    public static IQueryable<T> Paginate<T>(
+        this IQueryable<T> query,
+        int pageNumber,
+        int pageSize)
+    {
+        return query
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize);
+    }
 }
