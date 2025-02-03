c8044057d8334535f6efa0c1345fad45651b6e67
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-03 10:45:54 +0200

    Add RegEx for user entity

--- src/TABP.Application/Validators/User/UserValidator.cs
+++ src/TABP.Application/Validators/User/UserValidator.cs
@@ -1,6 +1,5 @@
 using FluentValidation;
 using TABP.Domain.Abstractions.Repositories;
-using TABP.Domain.Abstractions.Services;
 using TABP.Domain.Constants.User;
 using TABP.Domain.Models.User;
 
@@ -8,12 +7,14 @@ namespace TABP.Application.Validators.User;
 
 internal class UserValidator : AbstractValidator<UserDTO>
 {
-    public UserValidator(IUserRepository userRepository) // add regex bro.
+    public UserValidator(IUserRepository userRepository)
     {
         RuleFor(user => user.Username)
             .NotNull()
             .Length(UserConstants.MinUsernameLength, UserConstants.MaxUsernameLength)
-            .WithMessage("{PropertyName} has invalid length or format.") // maybe give a better message?
+            .Matches(@"^[a-zA-Z0-9_]+$")
+                    .WithMessage("{PropertyName} must be alphanumeric (underscores allowed).")
+            .WithMessage("{PropertyName} has invalid length or format.")
             .MustAsync(async (username, cancellation) => 
                 !await userRepository.ExistsByUsernameAsync(username))
             .WithMessage("{PropertyName} already exists.");
@@ -21,16 +22,21 @@ internal class UserValidator : AbstractValidator<UserDTO>
         RuleFor(user => user.FirstName)
             .NotNull()
             .Length(UserConstants.MinFirstNameLength, UserConstants.MaxFirstNameLength)
-            .WithMessage("{PropertyName} has invalid length or format."); // maybe give a better message?
+            .WithMessage("{PropertyName} has invalid length or format.")
+            .Matches(@"^[A-Za-z]+$")
+                    .WithMessage("{PropertyName} must contain only letters.");
 
         RuleFor(user => user.LastName)
             .NotNull()
             .Length(UserConstants.MinLastNameLength, UserConstants.MaxLastNameLength)
-            .WithMessage("{PropertyName} has invalid length or format."); // maybe give a better message?
+            .WithMessage("{PropertyName} has invalid length or format.")
+            .Matches(@"^[A-Za-z]+$")
+                    .WithMessage("{PropertyName} must contain only letters.");
 
         RuleFor(user => user.Password)
             .NotNull()
             .Length(UserConstants.MinPasswordLength, UserConstants.MaxPasswordLength)
-            .WithMessage("{PropertyName} has invalid length or format."); // maybe give a better message? // add regex later.
+            .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':""\\|,.<>\/?]).+$")
+                    .WithMessage("{PropertyName} must have at least one uppercase letter, one lowercase letter, one digit, and one special character.");
     }
 }
