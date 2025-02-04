6600ee12f64afe94e1ca3a209704ed869bedb219
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-04 09:39:30 +0200

    Add Booking Sort Validator

---
+++ src/TABP.Application/Validators/Sort/RoomBookingSortQueryValidator.cs
@@ -0,0 +1,57 @@
+using FluentValidation;
+using TABP.Domain.Models.RoomBooking;
+
+namespace TABP.Application.Validators.Sort;
+
+public class RoomBookingSortQueryValidator : AbstractValidator<BookingSortQuery>
+{
+    private static readonly HashSet<string> SortByOptions = 
+        new(StringComparer.OrdinalIgnoreCase)
+    {
+        "TotalPrice",
+        "CheckInDate",
+        "CheckOutDate"
+    };
+
+    private static readonly HashSet<string> SortByOptionsForAdmin = 
+        new(SortByOptions, StringComparer.OrdinalIgnoreCase)
+    {
+        "CreationDate",
+        "ModificationDate"
+    };
+
+    private static readonly HashSet<string> SortOrderOptions = 
+        new(StringComparer.OrdinalIgnoreCase)
+    {
+        "Asc", "Desc"
+    };
+
+    public RoomBookingSortQueryValidator()
+    {
+        
+         RuleFor(query => query.SortBy)
+            .Must((query, sortBy) => BeValidSortByOption(sortBy, query.IsAdmin))
+            .WithMessage((query, sortBy) => 
+                query.IsAdmin 
+                    ? $"Invalid sort field: '{sortBy}'. Valid fields are: {string.Join(", ", SortByOptionsForAdmin)}"
+                    : $"Invalid sort field: '{sortBy}'. Valid fields are: {string.Join(", ", SortByOptions)}");
+
+        RuleFor(query => query.SortOrder)
+            .Must(BeValidSortOrderOption)
+            .WithMessage("Invalid sort order: '{PropertyValue}'. Use 'asc' or 'desc'.");
+    }
+
+    private bool BeValidSortByOption(
+        string sortBy,
+        bool isAdmin)
+    {
+        return string.IsNullOrEmpty(sortBy) || 
+            (isAdmin 
+                ? SortByOptionsForAdmin.Contains(sortBy) : 
+                SortByOptions.Contains(sortBy));
+    }
+
+    private bool BeValidSortOrderOption(string sortOrder) =>
+        string.IsNullOrEmpty(sortOrder) || 
+            SortOrderOptions.Contains(sortOrder);
+}
