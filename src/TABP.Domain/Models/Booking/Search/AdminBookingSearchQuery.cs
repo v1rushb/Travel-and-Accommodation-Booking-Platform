af740df92af5d934a60973c45cae4e0adde2fc1c
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-04 09:42:41 +0200

    Add ToString override method for Booking  Models for logging

--- src/TABP.Domain/Models/Booking/Search/AdminBookingSearchQuery.cs
+++ src/TABP.Domain/Models/Booking/Search/AdminBookingSearchQuery.cs
@@ -10,4 +10,14 @@ public class AdminBookingSearchQuery
     public int RoomNumber { get; set; } = 1;
     public Guid HotelId { get; set; }
     public Guid? UserId { get; set; }
+
+    public override string ToString() =>
+    @$"
+            CheckInDate: {CheckInDate?.ToString("yyyy-MM-dd") ?? "None"} - CheckOutDate: {CheckOutDate?.ToString("yyyy-MM-dd") ?? "None"},
+            MinPrice: {MinPrice}{(MinPrice == 0 ? " (default)" : "")}, 
+            MaxPrice: {MaxPrice}{(MaxPrice == decimal.MaxValue ? " (default)" : "")}, 
+            Notes: {Notes ?? "None"}, 
+            RoomNumber: {RoomNumber}{(RoomNumber == 1 ? " (default)" : "")}, 
+            HotelId: {HotelId}, 
+            UserId: {UserId?.ToString() ?? "None"}";
 }
--- src/TABP.Domain/Models/Booking/Search/BookingSearchQuery.cs
+++ src/TABP.Domain/Models/Booking/Search/BookingSearchQuery.cs
@@ -9,4 +9,13 @@ public class BookingSearchQuery
     public string Notes { get; set; }
     public int RoomNumber { get; set; } = 1;
     public Guid HotelId { get; set; }
+
+    public override string ToString() =>
+    @$"
+            CheckInDate: {CheckInDate?.ToString("yyyy-MM-dd") ?? "None"} - CheckOutDate: {CheckOutDate?.ToString("yyyy-MM-dd") ?? "None"},
+            MinPrice: {MinPrice}{(MinPrice == 0 ? " (default)" : "")}, 
+            MaxPrice: {MaxPrice}{(MaxPrice == decimal.MaxValue ? " (default)" : "")}, 
+            Notes: {Notes ?? "None"}, 
+            RoomNumber: {RoomNumber}{(RoomNumber == 1 ? " (default)" : "")},    
+            HotelId: {HotelId}";
 }
