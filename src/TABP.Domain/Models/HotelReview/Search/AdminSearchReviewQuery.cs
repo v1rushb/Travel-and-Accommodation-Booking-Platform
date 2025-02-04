f726920d58f8a9cae450060dd952e7fa1a737e40
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-04 09:59:37 +0200

    Add ToString override method for Hotel-Review Models for logging

--- src/TABP.Domain/Models/HotelReview/Search/AdminSearchReviewQuery.cs
+++ src/TABP.Domain/Models/HotelReview/Search/AdminSearchReviewQuery.cs
@@ -1,13 +1,30 @@
 namespace TABP.Domain.Models.HotelReview.Search;
 
-public class AdminReviewSearchQuery
-{
+ public class AdminReviewSearchQuery
+    {
+        public string? SearchTerm { get; set; }
+        public IEnumerable<int>? Rating { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public Guid HotelId { get; set; }
+        public Guid? UserId { get; set; }
 
-    public string SearchTerm { get; set; }
-    public IEnumerable<int>? Rating { get; set; }
-    public DateTime CreationDate { get; set; }
-    public DateTime? StartDate { get; set; }
-    public DateTime? EndDate { get; set; }
-    public Guid HotelId { get; set; }
-    public Guid? UserId { get; set; }
+        public override string ToString()
+        {
+            var searchTermDisplay = !string.IsNullOrEmpty(SearchTerm) 
+                ? $"'{SearchTerm}'" 
+                : "None";
+
+            return @$"
+                        SearchTerm: {searchTermDisplay},
+                        Rating: {(Rating != null ? string.Join(", ", Rating) : "None")},
+                        CreationDate: {CreationDate},
+                        StartDate: {StartDate?.ToString("yyyy-MM-dd") ?? "None"},
+                        EndDate: {EndDate?.ToString("yyyy-MM-dd") ?? "None"},
+                        HotelId: {HotelId}, 
+                        UserId: {UserId?.ToString() ?? "None"}";
+            }
+
+
 }
--- src/TABP.Domain/Models/HotelReview/Search/SearchReviewQuery.cs
+++ src/TABP.Domain/Models/HotelReview/Search/SearchReviewQuery.cs
@@ -11,4 +11,20 @@ public class ReviewSearchQuery
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
     public Guid HotelId { get; set; }
+
+    public override string ToString()
+    {
+        var searchTermDisplay = !string.IsNullOrEmpty(SearchTerm) 
+            ? $"'{SearchTerm}'" 
+            : "None";
+
+        return @$"
+                SearchTerm: {searchTermDisplay},
+                Rating: {(Rating != null ? string.Join(", ", Rating) : "None")},
+                CreationDate: {CreationDate},
+                StartDate: {StartDate?.ToString("yyyy-MM-dd") ?? "None"},
+                EndDate: {EndDate?.ToString("yyyy-MM-dd") ?? "None"},
+                HotelId: {HotelId}";
+    }
+
 }
