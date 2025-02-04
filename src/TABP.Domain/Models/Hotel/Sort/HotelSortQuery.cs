cd34a150cabc99afae0e3269346f3a505caf0569
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-04 10:05:03 +0200

    Add Hotel Sort Query Model

--- src/TABP.Domain/Models/Hotel/Sort/HotelSortQuery.cs
+++ src/TABP.Domain/Models/Hotel/Sort/HotelSortQuery.cs
@@ -5,4 +5,10 @@ public class HotelSortQuery
     public string SortBy { get; set;} = "Name";
     public string SortOrder { get; set;} = "Asc";
     public bool IsAdmin { get; set; } = false;
+
+    public override string ToString() =>
+        @$"
+                SortBy: {SortBy},
+                SortOrder: {SortOrder},
+                IsAdmin: {IsAdmin}";
 }
