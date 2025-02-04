1dc7bdd7c733e3c3455a69bffc7183c973e3f4a7
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-04 09:37:15 +0200

    Add ToString override method for Room  Models for logging

--- src/TABP.Domain/Models/Room/Search/RoomSearchQuery.cs
+++ src/TABP.Domain/Models/Room/Search/RoomSearchQuery.cs
@@ -11,4 +11,18 @@ public class RoomSearchQuery
     public int MaxChildrenCapacity { get; set; } = int.MaxValue;
     public int MinPricePerNight { get; set; } = 0;
     public int MaxPricePerNight { get; set; } = int.MaxValue;
+
+    public override string ToString() =>
+    @$"
+                MinNumber: {MinNumber}{(MinNumber == 1 ? " (default)" : "")}, 
+                MaxNumber: {MaxNumber}{(MaxNumber == int.MaxValue ? " (default)" : "")}, 
+                RoomType: {(roomType != null && roomType.Any() ? string.Join(", ", roomType) : "None")}, 
+                MinAdultsCapacity: {MinAdultsCapacity}{(MinAdultsCapacity == 2 ? " (default)" : "")}, 
+                MaxAdultsCapacity: {MaxAdultsCapacity}{(MaxAdultsCapacity == int.MaxValue ? " (default)" : "")}, 
+                MinChildrenCapacity: {MinChildrenCapacity}{(MinChildrenCapacity == 0 ? " (default)" : "")}, 
+                MaxChildrenCapacity: {MaxChildrenCapacity}{(MaxChildrenCapacity == int.MaxValue ? " (default)" : "")}, 
+                MinPricePerNight: {MinPricePerNight}{(MinPricePerNight == 0 ? " (default)" : "")}, 
+                MaxPricePerNight: {MaxPricePerNight}{(MaxPricePerNight == int.MaxValue ? " (default)" : "")}";
+
+
 }
