e0a3888815ab59a215ebcd743deda2aa2828ea66
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-06 14:03:20 +0200

    Add Hotel Admin Integration Tests

---
+++ Tests/TABP.API.IntegrationTests/HotelAdminControllerTests.cs
@@ -0,0 +1,214 @@
+using System.Net;
+using System.Net.Http.Json;
+using AutoFixture;
+using FluentAssertions;
+using Microsoft.AspNetCore.JsonPatch;
+using TABP.Domain.Models.Hotel;
+using TABP.Domain.Models.Hotels;
+
+namespace TABP.API.IntegrationTests;
+
+public class HotelAdminController : IClassFixture<HotelBookingFactory>
+{
+    private readonly HttpClient _guest;
+    private readonly HttpClient _user;
+    private readonly HttpClient _admin;
+    private readonly HttpClient _loggedOutAdmin;
+    private readonly Fixture _fixture = new();
+    
+    public HotelAdminController(HotelBookingFactory _factory)
+    {
+        _guest = _factory.GetGuestClient();
+        _user = _factory.GetAuthenticatedUserClientAsync().Result;
+        _admin = _factory.GetAuthenticatedAdminClientAsync().Result;
+        _loggedOutAdmin = _factory.GetLoggedOutAdminClientAsync().Result;
+    }
+
+    [Fact]
+    public async Task Requests_ReturnsUnauthorizedFor_Guests()
+    {
+        await ExecuteAdminRequestTests(_guest, HttpStatusCode.Unauthorized);
+    }
+    
+    [Fact]
+    public async Task Requests_ShouldBeAuthorizedFor_Admins()
+    {
+        await ExecuteAdminRequestTests(_admin, isGuest: false);
+    }
+
+    private async Task ExecuteAdminRequestTests(
+        HttpClient client,
+        HttpStatusCode? expectedStatusCode = null,
+        bool isGuest = true)
+    {
+        // Arrange
+        var searchString = string.Empty;
+        var newHotel = new HotelForCreationDTO();
+        var hotelId = new Guid();
+        var hotelPatch = new JsonPatchDocument<HotelForUpdateDTO>();
+
+        var statusCodes = new List<HttpStatusCode>();
+
+        // Act
+        var searchStatusCode =
+            (await client
+                .GetAsync($"api/admin/hotels/search?query={searchString}"))
+                    .StatusCode;
+
+        var postStatusCode =
+            (await client
+                .PostAsJsonAsync("api/admin/hotels", newHotel))
+                    .StatusCode;
+
+        var deleteStatusCode = (await client
+            .DeleteAsync($"api/admin/hotels/{hotelId}"))
+                .StatusCode;
+
+        var patchStatusCode =
+            (await client
+                .PatchAsJsonAsync($"api/admin/hotels/{hotelId}", hotelPatch))
+                    .StatusCode;
+
+        statusCodes.AddRange(
+            [searchStatusCode, postStatusCode, deleteStatusCode, patchStatusCode]);
+
+        // Assert
+        if(isGuest) {
+            statusCodes.ForEach(statusCode => 
+                statusCode
+                    .Should().Be(expectedStatusCode));
+        } else {
+            statusCodes.ForEach(statusCode =>
+                statusCode.Should().NotBe(HttpStatusCode.Unauthorized)
+                    .And.NotBe(HttpStatusCode.Forbidden));
+        }
+    }
+
+    [Fact]
+    public async Task AuthorizedPostRequest_ShouldReturnBadRequestFor_InvalidBody()
+    {
+        // Arrange
+        var hotelName = _fixture.Create<string>();
+        var newHotel = new HotelForCreationDTO
+        {
+            Name = hotelName
+        };
+
+        // Act
+        var postStatusCode = (await _admin
+            .PostAsJsonAsync("/api/admin/hotels", newHotel))
+                .StatusCode;
+
+        // Assert
+        postStatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task Requests_ReturnForbiddenFor_LoggedOutAdmins()
+    {
+        await ExecuteLoggedOutAdminRequestTests(_loggedOutAdmin);
+    }
+
+    private async Task ExecuteLoggedOutAdminRequestTests(
+        HttpClient client)
+    {
+        // Arrange
+        var searchString = string.Empty;
+        var newHotel = new HotelForCreationDTO();
+        var hotelId = new Guid();
+        var hotelPatch = new JsonPatchDocument<HotelForUpdateDTO>();
+
+        var statusCodes = new List<HttpStatusCode>();
+
+        // Act
+        var searchStatusCode =
+            (await client
+                .GetAsync($"api/admin/hotels/search?query={searchString}"))
+                    .StatusCode;
+
+        var postStatusCode =
+            (await client
+                .PostAsJsonAsync("api/admin/hotels", newHotel))
+                    .StatusCode;
+
+        var deleteStatusCode = (await client
+            .DeleteAsync($"api/admin/hotels/{hotelId}"))
+                .StatusCode;
+
+        var patchStatusCode =
+            (await client
+                .PatchAsJsonAsync($"api/hotels/{hotelId}", hotelPatch))
+                    .StatusCode;
+
+        statusCodes.AddRange(
+            [searchStatusCode, postStatusCode, deleteStatusCode, patchStatusCode]);
+
+        // Assert
+        statusCodes.ForEach(statusCode => statusCode
+            .Should().Be(HttpStatusCode.Forbidden));
+    }
+
+    [Fact]
+    public async Task UserRequests_ShouldReturnForbiddenFor_AdminEndpoints()
+    {
+        await ExecuteUserRequestTests(_user, HttpStatusCode.Forbidden);
+    }
+
+    private async Task ExecuteUserRequestTests(
+        HttpClient client,
+        HttpStatusCode expectedStatusCode)
+    {
+        // Arrange
+        var searchString = string.Empty;
+        var newHotel = new HotelForCreationDTO();
+        var hotelId = new Guid();
+        var hotelPatch = new JsonPatchDocument<HotelForUpdateDTO>();
+
+        var statusCodes = new List<HttpStatusCode>();
+
+        // Act
+        var searchStatusCode =
+            (await client
+                .GetAsync($"api/admin/hotels/search?query={searchString}"))
+                    .StatusCode;
+
+        var postStatusCode =
+            (await client
+                .PostAsJsonAsync("api/admin/hotels", newHotel))
+                    .StatusCode;
+
+        var deleteStatusCode = (await client
+            .DeleteAsync($"api/admin/hotels/{hotelId}"))
+                .StatusCode;
+
+        var patchStatusCode =
+            (await client
+                .PatchAsJsonAsync($"api/admin/hotels/{hotelId}", hotelPatch))
+                    .StatusCode;
+
+        statusCodes.AddRange(
+            [searchStatusCode, postStatusCode, deleteStatusCode, patchStatusCode]);
+
+        // Assert
+        statusCodes.ForEach(statusCode =>
+            statusCode.Should().Be(expectedStatusCode)
+                .And.NotBe(HttpStatusCode.Unauthorized));
+    }
+
+    [Fact]
+    public async Task Search_ShouldReturnOkFor_Admins()
+    {
+        // Arrange
+        var searchTerm = _fixture.Create<string>();
+
+
+        // Act
+        var statusCode = (await _admin
+            .GetAsync($"/api/admin/hotels/search?searchTerm={searchTerm}"))
+                .StatusCode;
+
+
+        // Assert
+        statusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
