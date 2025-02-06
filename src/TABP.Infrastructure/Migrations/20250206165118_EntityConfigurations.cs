fdfb3db64e1e1c53c7c46517457f635c55b05576
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-06 20:14:58 +0200

    Add Migrations for Tables & Entity Configurations

---
+++ src/TABP.Infrastructure/Migrations/20250206165118_EntityConfigurations.cs
@@ -0,0 +1,252 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TABP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EntityConfigurations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerName",
+                table: "Hotels",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Hotels",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DetailedDescription",
+                table: "Hotels",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BriefDescription",
+                table: "Hotels",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Feedback",
+                table: "HotelReviews",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "Discounts",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cities",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CountryName",
+                table: "Cities",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelVisits_CreationDate",
+                table: "HotelVisits",
+                column: "CreationDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Discounts_EndingDate",
+                table: "Discounts",
+                column: "EndingDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Discounts_StartingDate",
+                table: "Discounts",
+                column: "StartingDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Username",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_HotelVisits_CreationDate",
+                table: "HotelVisits");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Discounts_EndingDate",
+                table: "Discounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Discounts_StartingDate",
+                table: "Discounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerName",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DetailedDescription",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BriefDescription",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Feedback",
+                table: "HotelReviews",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "Discounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CountryName",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
---
+++ src/TABP.Infrastructure/Migrations/20250206165146_AddViews.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TABP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddViews : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                CREATE VIEW vw_AvailableRooms AS
+                SELECT 
+                    R.Number,
+                    R.Type,
+                    R.AdultsCapacity,
+                    R.ChildrenCapacity,
+                    R.PricePerNight,
+                    R.HotelId,
+                    H.Name AS HotelName,
+                    H.StarRating,
+                    R.CreationDate,
+                    R.ModificationDate
+                FROM Rooms R
+                JOIN Hotels H ON R.HotelId = H.Id
+                WHERE NOT EXISTS (
+                    SELECT 1
+                    FROM Bookings B
+                    WHERE B.RoomId = R.Id
+                    AND GETDATE() BETWEEN B.StartingDate AND B.EndingDate
+                );
+                """);
+
+            migrationBuilder.Sql("""
+                CREATE VIEW vw_HotelRevenue AS
+                SELECT 
+                    H.Id AS HotelId,
+                    H.Name,
+                    H.BriefDescription,
+                    H.DetailedDescription,
+                    H.StarRating,
+                    H.OwnerName,
+                    H.Geolocation,
+                    H.CreationDate,
+                    H.ModificationDate,
+                    H.CityId,
+                    C.Name AS CityName,
+                    C.CountryName AS CityCountryName,
+                    SUM(R.PricePerNight * DATEDIFF(DAY, B.StartingDate, B.EndingDate)) AS TotalRevenue
+                FROM Hotels H
+                JOIN Rooms R ON H.Id = R.HotelId
+                JOIN Bookings B ON R.Id = B.RoomId
+                JOIN Cities C ON H.CityId = C.Id
+                WHERE B.EndingDate < GETDATE()
+                GROUP BY 
+                    H.Id, 
+                    H.Name, 
+                    H.BriefDescription, 
+                    H.DetailedDescription, 
+                    H.StarRating, 
+                    H.OwnerName, 
+                    H.Geolocation, 
+                    H.CreationDate, 
+                    H.ModificationDate, 
+                    H.CityId,
+                    C.Name,
+                    C.CountryName;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW vw_AvailableRooms, vw_HotelRevenue");
+        }
+    }
+}
