b542b2bd2592cbb2a8ad9cc99b5eae4cd9d22dbc
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-06 14:08:10 +0200

    Lower the required key length

--- src/TABP.Domain/Constants/JWT/JWTConstants.cs
+++ src/TABP.Domain/Constants/JWT/JWTConstants.cs
@@ -2,6 +2,6 @@ namespace TABP.Domain.Constants.JWT;
 
 public static class JWTConstants
 {
-    public const int MinKeyLength = 64;
+    public const int MinKeyLength = 32;
     public const int MinExpirationTimeMinutes = 1;
 }
