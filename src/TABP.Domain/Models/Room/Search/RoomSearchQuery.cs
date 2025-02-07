398f0d78186eca383d226d128bc1f70dbb4c18bb
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-07 13:56:40 +0200

    Change default values for Room search Query

--- src/TABP.Domain/Models/Room/Search/RoomSearchQuery.cs
+++ src/TABP.Domain/Models/Room/Search/RoomSearchQuery.cs
@@ -2,7 +2,7 @@ namespace TABP.Domain.Models.Room.Search;
 
 public class RoomSearchQuery
 {
-    public int MinNumber { get; set; } = 1;
+    public int MinNumber { get; set; } = 0;
     public int MaxNumber { get; set; } = int.MaxValue;
     public IEnumerable<int> roomType { get; set; } 
     public int MinAdultsCapacity { get; set; } = 2;
