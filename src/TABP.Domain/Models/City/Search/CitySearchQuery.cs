e3e2d0c61c6ac0cc8153d3c73942ca0e8271f762
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-08 11:00:01 +0200

    Add Id searching capability

--- src/TABP.Domain/Models/City/Search/CitySearchQuery.cs
+++ src/TABP.Domain/Models/City/Search/CitySearchQuery.cs
@@ -4,9 +4,15 @@ public class CitySearchQuery
 {
     public string? SearchTerm { get; set; }
     public string? Country { get; set; }
+    public Guid? Id { get; set; } = null;
 
     public override string ToString() =>
     @$"
             SearchTerm: {SearchTerm ?? "None"},
-            Country: {Country ?? "None"}";
+            Country: {Country ?? "None"}
+            Id: {getIdStateString()}";
+
+    private string getIdStateString() =>
+        Id.HasValue ? Id.Value.ToString() : "None";
+
 }
