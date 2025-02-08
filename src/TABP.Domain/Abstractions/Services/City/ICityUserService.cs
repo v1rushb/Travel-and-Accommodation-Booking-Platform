5c45e01273e62ceb0ec9e9ae035ac89f5e74a895
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-08 11:00:52 +0200

    Add Sorting Capabilies for City

--- src/TABP.Domain/Abstractions/Services/City/ICityUserService.cs
+++ src/TABP.Domain/Abstractions/Services/City/ICityUserService.cs
@@ -1,9 +1,13 @@
 using TABP.Domain.Models.City.Search;
+using TABP.Domain.Models.City.Sort;
 using TABP.Domain.Models.Pagination;
 
 namespace TABP.Domain.Abstractions.Services.City;
 
 public interface ICityUserService
 {
-    Task<IEnumerable<CitySearchResponseDTO>> SearchAsync(CitySearchQuery query, PaginationDTO pagination);
+    Task<IEnumerable<CitySearchResponseDTO>> SearchAsync(
+        CitySearchQuery query,
+        PaginationDTO pagination,
+        CitySortQuery sortQuery);
 }
