ec274fb55826de2ea5e88a08c506ba694e20185d
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-08 11:04:10 +0200

    Add Sort for city user queries

--- src/TABP.API/Controllers/CitiesController.cs
+++ src/TABP.API/Controllers/CitiesController.cs
@@ -5,6 +5,7 @@ using TABP.Domain.Models.Pagination;
 using TABP.Domain.Models.City.Search;
 using TABP.API.Extensions;
 using TABP.Domain.Abstractions.Services.City;
+using TABP.Domain.Models.City.Sort;
 
 namespace TABP.API.Controllers;
 
@@ -24,12 +25,14 @@ public class CitiesController : ControllerBase
     [HttpGet("search")]
     public async Task<IActionResult> SearchAndFilterCitiesAsync(
         [FromQuery] PaginationDTO pagination,
-        [FromQuery] CitySearchQuery query)
+        [FromQuery] CitySearchQuery query,
+        [FromQuery] CitySortQuery sortQuery)
     {
         var result = await _cityUserService
             .SearchAsync(
                 query,
-                pagination
+                pagination,
+                sortQuery
             );
 
         var citySize = result.Count();
--- src/TABP.Application/Services/City/CityUserService.cs
+++ src/TABP.Application/Services/City/CityUserService.cs
@@ -2,9 +2,11 @@ using AutoMapper;
 using FluentValidation;
 using Microsoft.Extensions.Logging;
 using TABP.Application.Filters.ExpressionBuilders;
+using TABP.Application.Sorting.ExpressionBuilders;
 using TABP.Domain.Abstractions.Repositories;
 using TABP.Domain.Abstractions.Services.City;
 using TABP.Domain.Models.City.Search;
+using TABP.Domain.Models.City.Sort;
 using TABP.Domain.Models.Pagination;
 
 namespace TABP.Application.Services.City;
@@ -16,35 +18,57 @@ public class CityUserService : ICityUserService
     private readonly IMapper _mapper;
     private readonly ILogger<CityUserService> _logger;
     private readonly ICurrentUserService _currentUserService;
+    private readonly IValidator<CitySortQuery> _sortValidator;
 
     public CityUserService(
         ICityRepository cityRepository,
         IValidator<PaginationDTO> paginationValidator,
         IMapper mapper,
         ILogger<CityUserService> logger,
-        ICurrentUserService currentUserService)
+        ICurrentUserService currentUserService,
+        IValidator<CitySortQuery> sortValidator)
     {
         _cityRepository = cityRepository;
         _paginationValidator = paginationValidator;
         _mapper = mapper;
         _logger = logger;
         _currentUserService = currentUserService;
+        _sortValidator = sortValidator;
     }
 
-    public async Task<IEnumerable<CitySearchResponseDTO>> SearchAsync(CitySearchQuery query, PaginationDTO pagination)
+    public async Task<IEnumerable<CitySearchResponseDTO>> SearchAsync(
+        CitySearchQuery query,
+        PaginationDTO pagination,
+        CitySortQuery sortQuery)
     {
         _paginationValidator.ValidateAndThrow(pagination);
+        _sortValidator.ValidateAndThrow(sortQuery);
+
 
         var expression = CityExpressionBuilder.Build(query);
+        var orderByDelegate = CitySortExpressionBuilder
+            .GetSortDelegate(sortQuery);
+
         var cities = await _cityRepository.SearchAsync(
             expression,
             pagination.PageNumber,
-            pagination.PageSize);
+            pagination.PageSize,
+            orderByDelegate
+            );
 
-        _logger.LogInformation(
-            "Searching for Cities with query {@CitySearchQuery} by User {UserId}", 
-            query, 
-            _currentUserService.GetUserId());
+            _logger.LogInformation(
+            @"
+                Searching For Cities with Search query: {SearchQuery}
+                With Sorting Query: {CitySortQuery}
+                PageNumber: {PageNumber}
+                PageSize: {PageSize}
+                By User With Id: {UserId}",
+
+                query,
+                sortQuery,
+                pagination.PageNumber,
+                pagination.PageSize,
+                _currentUserService.GetUserId());
         
         return _mapper.Map<IEnumerable<CitySearchResponseDTO>>(cities);
     }
