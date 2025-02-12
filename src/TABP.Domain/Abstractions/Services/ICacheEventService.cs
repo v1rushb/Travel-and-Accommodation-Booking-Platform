dfd6aaef0d7077db3d3674ca88756d6b7365c76c
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-12 13:01:30 +0200

    Cleaning minor Refactor

--- src/TABP.Domain/Abstractions/Services/ICacheEventService.cs
+++ src/TABP.Domain/Abstractions/Services/ICacheEventService.cs
@@ -2,5 +2,8 @@ namespace TABP.Domain.Abstractions.Services;
 
 public interface ICacheEventService
 {
-    Task ScheduleExpirationAsync(string key, TimeSpan expiration, Func<Task> onExpirationCallback);
+    Task ScheduleExpirationAsync(
+        string key,
+        TimeSpan expiration,
+        Func<Task> onExpirationCallback);
 }
