145fd693073ecc2adb49b98df25a8fb6c0e94844
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-14 22:32:18 +0200

    Document Entity class

--- src/TABP.Domain/Abstractions/Entity.cs
+++ src/TABP.Domain/Abstractions/Entity.cs
@@ -1,6 +1,14 @@
 namespace TABP.Domain.Abstractions;
 
+/// <summary>
+/// Abstract class that serves as a base entity for all other entities in domain.
+/// It provides a unique identifier of type<see cref="Guid"/>
+/// </summary>
 public abstract class Entity
 {
+    /// <summary>
+    /// Gets or sets the unique identifier of the entity.
+    /// </summary>
+    /// <value>The unique identifier for this entity, represented as a<see cref="Guid"/>.</value>
     public Guid Id { get; set; }
 }
