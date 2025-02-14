86f55f0cf8b3bba58ace8dd5c0c153c69df825c4
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-14 22:32:35 +0200

    Document ICurrentUserService

--- src/TABP.Domain/Abstractions/Utilities/Injectable/ICurrentUserService.cs
+++ src/TABP.Domain/Abstractions/Utilities/Injectable/ICurrentUserService.cs
@@ -1,13 +1,30 @@
 using TABP.Domain.Enums;
 
+namespace TABP.Domain.Abstractions.Utilities.Injectable;
+
 /// <summary>
 /// Provides access to the current authenticated user's details, such as their ID and roles.
 /// </summary>
-namespace TABP.Domain.Abstractions.Utilities.Injectable;
-
 public interface ICurrentUserService
 {
+    /// <summary>
+    /// Retrieves the unique identifier of the current authenticated user.
+    /// </summary>
+    /// <returns>The user ID as a<see cref="Guid"/>.</returns>\
+    /// <exception cref="UnauthorizedAccessException">Thrown if the user is not authenticated.</exception>
     Guid GetUserId();
+
+
+    /// <summary>
+    /// Retrieves the list of roles assigned to the current authenticated user.
+    /// </summary>
+    /// <returns>A list of roles (<see cref="RoleType"/>) assigned to the user.</returns>
     List<RoleType> GetUserRoles();
+
+     /// <summary>
+    /// Checks whether the current authenticated user has a specified role.
+    /// </summary>
+    /// <param name="roleType">The role to check.</param>
+    /// <returns><c>true</c> if the user is in the specified role; otherwise, <c>false</c>.</returns>
     bool IsInRole(RoleType roleType);
 }
