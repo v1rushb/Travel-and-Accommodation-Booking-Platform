cc2c075a4f58ea3e82c661e23e101269e1d95fc5
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-15 18:16:11 +0200

    Minor Refactor

--- src/TABP.Domain/Models/Hotel/FeaturedHotelDTO.cs
+++ src/TABP.Domain/Models/Hotel/FeaturedHotelDTO.cs
@@ -1,9 +1,10 @@
+using TABP.Domain.Abstractions;
+
 namespace TABP.Domain.Models.Hotel;
 
-public class FeaturedHotelDTO
+public class FeaturedHotelDTO : Entity
 {
     public string Name { get; set; }
-    public Guid HotelId { get; set; }
     public decimal StarRating { get; set; }
     public int WeeklyBookings { get; set; }
 }
