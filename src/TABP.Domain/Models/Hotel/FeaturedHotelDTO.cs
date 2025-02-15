378cb62f5b5413f25f57cf3f5c5e14309628778b
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-15 18:55:59 +0200

    Add Visitors

--- src/TABP.Domain/Models/Hotel/FeaturedHotelDTO.cs
+++ src/TABP.Domain/Models/Hotel/FeaturedHotelDTO.cs
@@ -7,4 +7,6 @@ public class FeaturedHotelDTO : Entity
     public string Name { get; set; }
     public decimal StarRating { get; set; }
     public int WeeklyBookings { get; set; }
+    public int WeeklyVisits { get; set; }
+    public int UniqueVisitors { get; set; }
 }
--- src/TABP.Domain/Models/Hotel/VisitedHotelDTO.cs
+++ src/TABP.Domain/Models/Hotel/VisitedHotelDTO.cs
@@ -5,6 +5,7 @@ public class VisitedHotelDTO
     public Guid Id { get; set; }
     public string Name { get; set; }
     public int Visits { get; set; }
+    public int UniqueVisitors { get; set; } 
     public decimal StarRating { get; set; }
     public string CityName { get ; set; }
 }
