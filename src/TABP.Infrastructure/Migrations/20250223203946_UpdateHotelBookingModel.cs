f34f5481305fce6153a56e58e933728ee4166168
v1rushb <cs.bashar.herbawi@gmail.com>
2025-02-24 11:43:46 +0200

    Add Map views migration (BUG FIX)

---
+++ src/TABP.Infrastructure/Migrations/20250223203946_UpdateHotelBookingModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TABP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateHotelBookingModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomsWithAvailability");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RoomsWithAvailability",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AdultsCapacity = table.Column<int>(type: "int", nullable: false),
+                    ChildrenCapacity = table.Column<int>(type: "int", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HotelId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HotelName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false),
+                    ModificationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Number = table.Column<int>(type: "int", nullable: false),
+                    PricePerNight = table.Column<int>(type: "int", nullable: false),
+                    StarRating = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomsWithAvailability", x => x.Id);
+                });
+        }
+    }
+}
